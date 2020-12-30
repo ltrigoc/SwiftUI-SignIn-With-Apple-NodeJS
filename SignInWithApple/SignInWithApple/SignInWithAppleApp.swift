873f8f9ada9b000ab2ae85c08b8af792881490ca
luistrigoc <luistrigoc@hotmail.com>
2020-12-30 22:53:44 +0100

    Swift project initial

---
+++ SignInWithApple/SignInWithApple/SignInWithAppleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SignInWithAppleApp.swift
+//  SignInWithApple
+//
+//  Created by Luis Trigo on 30/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct SignInWithAppleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
